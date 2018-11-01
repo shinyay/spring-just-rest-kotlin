abbbdfeae7f15ff5e57c4cf73d867ff5b66d6c06
shinyay <shinya.com@gmail.com>
2018-11-01 22:42:51 +0900

    Initial Commit

---
+++ src/main/kotlin/io/pivotal/syanagihara/simple/SimpleApplication.kt
@@ -0,0 +1,11 @@
+package io.pivotal.syanagihara.simple
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SimpleApplication>(*args)
+}
