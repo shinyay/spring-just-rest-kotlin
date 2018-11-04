e3694f2b4f51c0c0cc32305c11cb11a28dd90575
shinyay <shinya.com@gmail.com>
2018-11-04 17:21:16 +0900

    Add Simple RestController which maps as /simple for GET

---
+++ src/main/kotlin/io/pivotal/syanagihara/simple/controller/SimpleController.kt
@@ -0,0 +1,15 @@
+package io.pivotal.syanagihara.simple.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/simple")
+class SimpleController {
+
+    @GetMapping
+    fun getMessages() : String {
+        return "Under Construction"
+    }
+}
