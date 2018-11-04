4ff54dcdbc4ee8feb9e2f6a3ff11ad3b558f46b4
shinyay <shinya.com@gmail.com>
2018-11-04 18:36:59 +0900

    Add REST GET Method for displaying Message data
    Message data is dummy

--- src/main/kotlin/io/pivotal/syanagihara/simple/controller/SimpleController.kt
+++ src/main/kotlin/io/pivotal/syanagihara/simple/controller/SimpleController.kt
@@ -1,15 +1,34 @@
 package io.pivotal.syanagihara.simple.controller
 
+import io.pivotal.syanagihara.simple.data.Message
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.text.SimpleDateFormat
+import java.util.*
 
 @RestController
 @RequestMapping("/simple")
 class SimpleController {
 
     @GetMapping
-    fun getMessages() : String {
-        return "Under Construction"
+    fun getMessages() : List<Message> {
+        return listOf(
+                Message(
+                        UUID.randomUUID().toString(),
+                        "First Message",
+                        "This is a 1st message on ${getDate()}."
+                ),
+                Message(UUID.randomUUID().toString(),
+                        "Second Message",
+                        "This is a 2nd message on ${getDate()}."
+                )
+        )
+    }
+
+    private fun getDate() : String {
+        val simpleDateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+        return simpleDateFormat.format(Date())
     }
 }
