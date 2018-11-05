4f1ede89e54083aae5c57f295fefcb6ad26c0791
shinyay <shinya.com@gmail.com>
2018-11-06 00:06:32 +0900

    Modify parameter has default parameter as blank

--- src/main/kotlin/io/pivotal/syanagihara/simple/data/Message.kt
+++ src/main/kotlin/io/pivotal/syanagihara/simple/data/Message.kt
@@ -1,5 +1,5 @@
 package io.pivotal.syanagihara.simple.data
 
-data class Message(var id: String,
+data class Message(var id: String = "",
                    var title: String,
                    var message: String)
