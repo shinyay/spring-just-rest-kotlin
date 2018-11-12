c4d98424ee3e4bb6bdd125ac4e5988293d0a48e0
shinyay <shinya.com@gmail.com>
2018-11-12 23:40:40 +0900

    Modify logic to delegate to service layer

--- src/main/kotlin/io/pivotal/syanagihara/simple/controller/SimpleController.kt
+++ src/main/kotlin/io/pivotal/syanagihara/simple/controller/SimpleController.kt
@@ -1,28 +1,21 @@
 package io.pivotal.syanagihara.simple.controller
 
 import io.pivotal.syanagihara.simple.data.Message
+import io.pivotal.syanagihara.simple.service.MessageService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 @RestController
 @RequestMapping("/messages")
-class SimpleController {
+class SimpleController() {
+
+    @Autowired
+    private lateinit var service: MessageService
 
     @GetMapping
-    fun getMessages() : List<Message> {
-        return listOf(
-                Message(
-                        UUID.randomUUID().toString(),
-                        "First Message",
-                        "This is a 1st message on ${getDate()}."
-                ),
-                Message(UUID.randomUUID().toString(),
-                        "Second Message",
-                        "This is a 2nd message on ${getDate()}."
-                )
-        )
-    }
+    fun getMessages() = service.getMessages()
 
     private fun getDate() : String {
         val simpleDateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
@@ -30,18 +23,11 @@ class SimpleController {
     }
 
     @PostMapping
-    fun insertMessage(@RequestBody message: Message) : Message {
-        message.id = UUID.randomUUID().toString()
-        return message
-    }
+    fun insertMessage(@RequestBody message: Message) : Message = service.insertMessage(message)
 
     @PutMapping
-    fun updateMessage(@RequestBody message: Message) : Message {
-        message.title += "UPDATED TITLE:${getDate()}"
-        message.message += "UPDATED MESSAGE:${getDate()}"
-        return message
-    }
+    fun updateMessage(@RequestBody message: Message) : Boolean = service.updateMessage(message)
 
     @DeleteMapping(value = ["/{id}"])
-    fun deleteMessage(@PathVariable(name = "id") id: String): Boolean = true
+    fun deleteMessage(@PathVariable(name = "id") id: String): Boolean = service.deleteMessage(id)
 }
