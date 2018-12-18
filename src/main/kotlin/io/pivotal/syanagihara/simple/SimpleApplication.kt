3dbe7f64795fb072af8f4cae7d8b8325a08c0928
shinyay <shinya.com@gmail.com>
2018-12-19 06:49:53 +0900

    Add ThreadPoolTaskExecutor for asynchronous function
    Used scope function - apply for initialization

--- src/main/kotlin/io/pivotal/syanagihara/simple/SimpleApplication.kt
+++ src/main/kotlin/io/pivotal/syanagihara/simple/SimpleApplication.kt
@@ -2,14 +2,32 @@ package io.pivotal.syanagihara.simple
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.core.task.TaskExecutor
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 
 @SpringBootApplication
 @EnableAsync
 class SimpleApplication {
 
-    fun main(args: Array<String>) {
-        runApplication<SimpleApplication>(*args)
+    @Bean
+    fun normalTaskExecutor(): TaskExecutor  = ThreadPoolTaskExecutor().apply {
+        corePoolSize = 1
+        maxPoolSize = 1
+        setQueueCapacity(5)
+        setWaitForTasksToCompleteOnShutdown(true)
     }
+
+    @Bean
+    fun prioritizedTaskExecutor(): TaskExecutor  = ThreadPoolTaskExecutor().apply {
+        corePoolSize = 1
+        maxPoolSize = 1
+        setQueueCapacity(5)
+        setWaitForTasksToCompleteOnShutdown(true)
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<SimpleApplication>(*args)
 }
