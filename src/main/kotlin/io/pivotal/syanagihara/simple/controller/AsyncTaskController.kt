03d28d70533138eac72a51eeef07ab680d3ddacf
shinyay <shinya.com@gmail.com>
2018-12-19 06:53:06 +0900

    Add controller class for AsyncService

---
+++ src/main/kotlin/io/pivotal/syanagihara/simple/controller/AsyncTaskController.kt
@@ -0,0 +1,24 @@
+package io.pivotal.syanagihara.simple.controller
+
+import io.pivotal.syanagihara.simple.service.AsyncTaskService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/async")
+class AsyncTaskController {
+
+    @Autowired
+    private lateinit var service: AsyncTaskService
+
+    @GetMapping("/high")
+    fun callPrioritizedTask() = service.prioritizedTask()
+
+    @GetMapping("/normal")
+    fun callNormalTask() = service.normalTask()
+
+
+
+}
