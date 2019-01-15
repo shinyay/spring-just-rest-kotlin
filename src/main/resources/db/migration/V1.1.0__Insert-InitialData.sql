2d6a2e9f58643a1f9f35e7604ccbc0ab2fcd80e6
shinyay <shinya.com@gmail.com>
2019-01-15 16:59:32 +0900

    Add Inser Data SQL for Flyway

---
+++ src/main/resources/db/migration/V1.1.0__Insert-InitialData.sql
@@ -0,0 +1,3 @@
+INSERT INTO message(id, title, message) VALUES ('7b23257c-e9d9-4d1e-ba79-01f8b8715ba9', 'INIT', 'Inserted by FLYWAY');
+
+INSERT INTO message(id, title, message) VALUES ('12345678-e9d9-4d1e-ba79-01f8b8715ba9', 'INIT', 'Inserted by FLYWAY');
