1c710870c5def6aa1a5ca91b2de7ab4bb4f49fb7
shinyay <shinya.com@gmail.com>
2019-02-19 21:26:12 +0900

    Modify test as JUnit5

--- src/test/kotlin/io/pivotal/syanagihara/simple/SimpleApplicationTests.kt
+++ src/test/kotlin/io/pivotal/syanagihara/simple/SimpleApplicationTests.kt
@@ -1,12 +1,9 @@
 package io.pivotal.syanagihara.simple
 
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SimpleApplicationTests {
 
 	@Test
