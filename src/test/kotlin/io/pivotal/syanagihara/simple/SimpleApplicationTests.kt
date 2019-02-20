f2e87eb994b1b03c519129414c9072ad9c805877
shinyay <shinya.com@gmail.com>
2019-02-20 19:33:28 +0900

    Modify test contents

--- src/test/kotlin/io/pivotal/syanagihara/simple/SimpleApplicationTests.kt
+++ src/test/kotlin/io/pivotal/syanagihara/simple/SimpleApplicationTests.kt
@@ -1,16 +1,13 @@
 package io.pivotal.syanagihara.simple
 
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SimpleApplicationTests {
 
 	@Test
-	fun contextLoads() {
+	fun `サンプルテスト`() {
 	}
 
 }
