5357ae361c96e117db1f04e2fa6846a54d726dfe
shinyay <shinya.com@gmail.com>
2019-07-05 21:17:16 +0900

    Add Database Query function

--- src/main/kotlin/io/pivotal/syanagihara/simple/repository/MessageRepository.kt
+++ src/main/kotlin/io/pivotal/syanagihara/simple/repository/MessageRepository.kt
@@ -1,6 +1,13 @@
 package io.pivotal.syanagihara.simple.repository
 
 import io.pivotal.syanagihara.simple.data.Message
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import java.util.*
 
-interface MessageRepository : CrudRepository<Message, String>
+interface MessageRepository : CrudRepository<Message, String> {
+
+    @Query("from Message m where m.id = ?1")
+    fun findMessageIdIs(id: String): Iterable<Message>
+
+}
